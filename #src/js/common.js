5fe41a36c1fed69aabd6142a14d71641a8c41ead
Kamarhinanton <Kamarhin_anton@ukr.net>
2022-09-16 14:55:33 +0300

    feat: add header mob

--- #src/js/common.js
+++ #src/js/common.js
@@ -33,10 +33,3 @@ const showSubmenu = () => {
     }
   }
 }
-;
-
-
-
-
-
-
--- #src/js/script.js
+++ #src/js/script.js
@@ -1,30 +1,5 @@
-window.addEventListener('DOMContentLoaded', function () {
+@@include('common.js');
 
-})
-
-// const bg = () => {
-//     document.querySelectorAll(".ibg").forEach(el => {
-//         if (el.querySelector('img')) {
-//             el.style.backgroundImage = 'url(' + el.querySelector('img').getAttribute('src') + ')';
-//             el.querySelector('img').style.display = 'none';
-//         }
-//     });
-// }
-
-// const scrollPage = () => {
-//     let scrollPosition;
-//     const header = document.querySelector('.header');
-//     const scrollChange = 1;
-
-//     const addClass = () => header.classList.add("hide")
-//     const removeClass = () => header.classList.remove("hide")
-
-//     window.addEventListener('scroll', function () {
-//         scrollPosition = window.scrollY;
-//         if (scrollPosition >= scrollChange) { addClass() }
-//         else { removeClass() }
-//     })
-// }
 
 
 
