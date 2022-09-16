9b8f7bdd06e8d37ffdd8186ab468ff434977e228
Kamarhinanton <Kamarhin_anton@ukr.net>
2022-09-16 15:55:00 +0300

    fix: setHeight js

--- docs/js/script.js
+++ docs/js/script.js
@@ -8,7 +8,17 @@ const addClassHeader = () => {
   const header = document.querySelector('.header__content');
   if (!!btn) {
     btn.onclick = () => {
-      header.classList.toggle('active')
+      if (header.classList.contains('active')) {
+        header.classList.remove('active')
+        document.querySelectorAll('.submenu>ul').forEach(item => {
+          item.style.maxHeight = '0';
+        })
+        document.querySelectorAll('.submenu').forEach(item => {
+          item.classList.remove('active')
+        })
+      } else {
+        header.classList.add('active')
+      }
     }
   }
 }
@@ -16,20 +26,30 @@ const addClassHeader = () => {
 const showSubmenu = () => {
   if (window.screen.width < 1200) {
     setTimeout(setHeight, 500)
+    window.addEventListener('resize', () => console.log('hi'))
 
     function setHeight() {
       let itemsLinks = document.querySelectorAll('.content-navigation__links>.submenu');
       if (itemsLinks.length > 0) {
         itemsLinks.forEach(item => {
           let itemsMenu = item.querySelectorAll('ul>li');
-          let submenuHeight = Array.from(itemsMenu).reduce((sum, current) => sum + current.offsetHeight, 0);
+          let submenuHeight = getHeight(itemsMenu)
           item.onclick = () => {
-            itemsMenu[0].parentNode.style.maxHeight = itemsMenu[0].parentNode.style.maxHeight === submenuHeight + 'px' ?
-              '' : submenuHeight + 'px';
+            checkHeight(itemsMenu[0], submenuHeight)
             itemsMenu[0].parentNode.parentNode.classList.toggle('active')
           }
         })
       }
+    }
+
+    function getHeight(items) {
+      let subHeight = Array.from(items).reduce((sum, current) =>
+        sum + current.offsetHeight, 0);
+      return subHeight
+    }
+
+    function checkHeight(item, height) {
+      item.parentNode.style.maxHeight = item.parentNode.style.maxHeight === height + 'px' ? '0' : height + 'px';
     }
   }
 }
